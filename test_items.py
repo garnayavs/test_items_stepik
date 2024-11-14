620fd89b5b21180ec16cd953aef00a8717750564
garnayavs <garnayavs@gmail.com>
2024-11-14 16:00:29 +0300

    add gitignore

--- test_items.py
+++ test_items.py
@@ -4,13 +4,13 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 
 
-def test_visibility_add_to_busket_button_(browser):
+def test_visibility_add_to_busket_button(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     try:
         WebDriverWait(browser, 10).until(
             EC.visibility_of_element_located((By.CLASS_NAME, 'btn-add-to-basket'))
             )
-        time.sleep(10)
+        time.sleep(10) # чтобы убедиться, что язык верный
     except:
         assert False, f'Кнопка "Добавить в корзину" не найдена.'
 
